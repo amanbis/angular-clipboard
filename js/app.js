c122e647cc1921a47d3bfbc7f41dcc128ff68442
amanbis <amanb91@gmail.com>
2015-08-11 16:30:13 +0700

    Add text input to add new title
    Title only shows up once the text for new title is submitted.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-var app = angular.module('clipboard', []);
+(function() {
+	var app = angular.module('clipboard', []);
+
+	app.controller('MainController', function() {
+		this.list = lists;
+		this.newTitle = '';
+		this.show = false;
+
+		this.addTitle = function() {
+			this.list.title = this.newTitle;
+			this.show = true;
+		};
+	});
+
+	var lists = { title: 'Hi' };
+
+})();
